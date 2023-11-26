601926c95897f4df82ae069cdb65dc018b3eb436
umair <umair.akhter@gnb.ca>
2023-11-26 08:55:30 +0500

    fixes

---
+++ api/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../openapi.json";
+
+const app = express();
+
+const options = {
+  explorer: true,
+};
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, options)
+);
+
+app.get("/api", (_, res) => {
+  res.end(`Hello! from home`);
+});
+
+app.post("/api/item", (req, res) => {
+  const data = req.body;
+  res.json({ data });
+});
+
+app.get("/api/item:itemId", (req, res) => {
+  const { itemId } = req.params;
+  res.end(`Item: ${itemId}`);
+});
+
+app.put("/api/item:itemId", (req, res) => {
+  const { itemId } = req.params;
+  res.end(`updated Item: ${itemId}`);
+});
+
+export default app;
